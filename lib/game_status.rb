6c982042a54ddc74530d118011b636d2e5a24f1d
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-11 19:30:52 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -70,6 +70,6 @@ def winner?(board)
   winningcombo= won?(board)
   puts board[winningcombo[0]]
 end
-wonboard = [" ", "", "X", " ", "X", " ", "X", " ", " "]
+wonboard = [" ", "", "O", " ", "O", " ", "O", " ", " "]
 
 puts winner?(wonboard)
